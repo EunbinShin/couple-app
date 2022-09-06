1549e18b5fd7327e518b7513f969b238dd2b388f
EunbinShin <38269665+EunbinShin@users.noreply.github.com>
2022-09-07 00:01:44 +0900

    image css추가

--- src/components/ImageItem.js
+++ src/components/ImageItem.js
@@ -1,7 +1,10 @@
+import './ImageItem.css'
+
 const ImageItem = (props) =>{
     return(
-        <img 
-            src='https://image-notepet.akamaized.net/resize/620x-/seimage/20190419%2Fc8cbf27c85aaeb4be4ed833aab1c7621.jpg'
+        <img className='image_item'
+            src={props.link}
+            alt={props.link}
         />
     )
 }
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,13 +1,15 @@
 import ImageItem from "./ImageItem"
 
 const ImageList = () => {
+    const test_url = 'https://image-notepet.akamaized.net/resize/620x-/seimage/20190419%2Fc8cbf27c85aaeb4be4ed833aab1c7621.jpg'
+
     return (
         <div>
-            <ImageItem></ImageItem>
-            <ImageItem></ImageItem>
-            <ImageItem></ImageItem>
-            <ImageItem></ImageItem>
-            <ImageItem></ImageItem>
+            <ImageItem link={test_url}></ImageItem>
+            <ImageItem link={test_url}></ImageItem>
+            <ImageItem link={test_url}></ImageItem>
+            <ImageItem link={test_url}></ImageItem>
+            <ImageItem link={test_url}></ImageItem>
         </div>
     )
 }
